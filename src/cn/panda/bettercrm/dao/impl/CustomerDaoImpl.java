365d9920fa2d1f86306a3d165d1ff92172927ceb
pandaTED <woscaizi@gmail.com>
2016-01-04 12:26:56 +0800

    commit

--- src/cn/panda/bettercrm/dao/impl/CustomerDaoImpl.java
+++ src/cn/panda/bettercrm/dao/impl/CustomerDaoImpl.java
@@ -2,42 +2,53 @@ package cn.panda.bettercrm.dao.impl;
 
 import java.util.List;
 
+import org.hibernate.Session;
 import org.springframework.transaction.annotation.Transactional;
 
 import cn.panda.bettercrm.dao.CustomerDao;
 import cn.panda.bettercrm.domain.Customer;
+import cn.panda.bettercrm.domain.User;
+import cn.panda.bettercrm.utils.HibernateUtils;
 
 @Transactional
 public class CustomerDaoImpl implements CustomerDao {
 	
+	HibernateUtils hUtils = new HibernateUtils();
+	Session session = hUtils.getCurrentSession();
+	
 	@Override
 	public void save(Customer customer) {
-		// TODO Auto-generated method stub
+		
+		session.save(customer);
 		
 	}
 
 	@Override
 	public void delete(Long id) {
-		// TODO Auto-generated method stub
+		
+		Customer customer = (Customer) session.get(Customer.class, id);
+		session.delete(customer);
 		
 	}
 
 	@Override
 	public void update(Customer customer) {
-		// TODO Auto-generated method stub
+		
+		session.update(customer);
 		
 	}
 
 	@Override
 	public Customer find(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return (Customer) session.get(Customer.class, id);
 	}
 
 	@Override
 	public List<Customer> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return session.createCriteria(Customer.class).list();
+		
 	}
 
 }
--- src/cn/panda/bettercrm/dao/impl/TrackingDaoImpl.java
+++ src/cn/panda/bettercrm/dao/impl/TrackingDaoImpl.java
@@ -2,42 +2,52 @@ package cn.panda.bettercrm.dao.impl;
 
 import java.util.List;
 
+import org.hibernate.Session;
 import org.springframework.transaction.annotation.Transactional;
 
 import cn.panda.bettercrm.dao.TrackingDao;
 import cn.panda.bettercrm.domain.Tracking;
+import cn.panda.bettercrm.utils.HibernateUtils;
 
 @Transactional
 public class TrackingDaoImpl implements TrackingDao {
 
+	HibernateUtils hUtils = new HibernateUtils();
+	Session session = hUtils.getCurrentSession();
+	
 	@Override
 	public void save(Tracking tracking) {
-		// TODO Auto-generated method stub
+		
+		session.save(tracking);
 		
 	}
 
 	@Override
 	public void delete(Long id) {
-		// TODO Auto-generated method stub
+		
+		Tracking tracking = (Tracking) session.get(Tracking.class, id);
+		session.delete(tracking);
 		
 	}
 
 	@Override
-	public void update(Tracking tracking) {
-		// TODO Auto-generated method stub
+	public void update(Tracking tracking) {	
 		
+		session.update(tracking);
 	}
 
 	@Override
 	public Tracking find(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return (Tracking) session.get(Tracking.class, id);
 	}
 
 	@Override
 	public List<Tracking> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return session.createCriteria(Tracking.class).list();
+		
+		
 	}
 
 }
